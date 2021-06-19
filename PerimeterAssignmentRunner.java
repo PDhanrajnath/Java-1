da6919f1d1138cfbed67ebd783f0f77e43a68b5b
Porika Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2021-06-19 15:05:28 +0530

    final commit

--- PerimeterAssignmentRunner.java
+++ PerimeterAssignmentRunner.java
@@ -144,11 +144,9 @@ public class PerimeterAssignmentRunner {
         PerimeterAssignmentRunner pr = new PerimeterAssignmentRunner();
 
         System.out.println("Assignment 1: Calculating information about shapes");
-        System.out.println("==================================================");
         pr.testPerimeter();
 
         System.out.println("\n\nAssignment 2: Processing multiple Shape files");
-        System.out.println("=============================================");
         pr.testPerimeterMultipleFiles();
         pr.testFileWithLargestPerimeter();
     }
